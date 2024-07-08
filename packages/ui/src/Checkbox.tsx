eb9cb4ea0a9f467358fd752c7496c7b5b6fe7bf5
unisol1020 <unisol1020@icloud.com>
2024-07-08 19:00:43 +0300

    [CLEANUP] Checkbox component

--- packages/ui/src/Checkbox.tsx
+++ packages/ui/src/Checkbox.tsx
@@ -1,7 +1,13 @@
-import {ComponentPropsWithoutRef, createContext, ElementRef, forwardRef, useContext} from 'react';
-import {GestureResponderEvent, Platform, Pressable, View} from 'react-native';
+import {
+  ComponentPropsWithoutRef,
+  createContext,
+  ElementRef,
+  forwardRef,
+  useContext,
+} from 'react';
+import { GestureResponderEvent, Platform, Pressable, View } from 'react-native';
 import { cn } from '../lib/utils';
-import {Check} from 'lucide-react-native';
+import { Check } from 'lucide-react-native';
 
 const CheckboxContext = createContext<{
   checked: boolean;
@@ -9,82 +15,6 @@ const CheckboxContext = createContext<{
   disabled?: boolean;
   nativeID?: string;
 } | null>(null);
-
-const CheckboxRoot = forwardRef<ElementRef<typeof Pressable>, ComponentPropsWithoutRef<typeof Pressable> & {
-  checked: boolean;
-  onCheckedChange: (checked: boolean) => void;
-  disabled?: boolean;
-}>(
-  ({ disabled = false, checked, onCheckedChange, nativeID, ...props }, ref) => {
-    return (
-      <CheckboxContext.Provider
-        value={{
-          disabled,
-          checked,
-          onCheckedChange,
-          nativeID,
-        }}
-      >
-        <CheckboxTrigger ref={ref} {...props} />
-      </CheckboxContext.Provider>
-    );
-  }
-);
-
-const CheckboxTrigger = forwardRef<ElementRef<typeof Pressable>, ComponentPropsWithoutRef<typeof Pressable>>(
-  ({ onPress: onPressProp, ...props }, ref) => {
-    const { disabled, checked, onCheckedChange, nativeID } = useCheckboxContext();
-
-    const onPress = (ev: GestureResponderEvent) => {
-      if (disabled) return;
-      const newValue = !checked;
-      onCheckedChange(newValue);
-      onPressProp?.(ev);
-    }
-
-    return (
-      <Pressable
-        ref={ref}
-        nativeID={nativeID}
-        aria-disabled={disabled}
-        role='checkbox'
-        aria-checked={checked}
-        onPress={onPress}
-        accessibilityState={{
-          checked,
-          disabled,
-        }}
-        disabled={disabled}
-        {...props}
-      />
-    );
-  }
-);
-
-const CheckboxIndicator = forwardRef<
-  ElementRef<typeof View>,
-  ComponentPropsWithoutRef<typeof View> & {
-  forceMount?: true | undefined;
-}
->(({ forceMount, ...props }, ref) => {
-  const { checked, disabled } = useCheckboxContext();
-
-  if (!forceMount) {
-    if (!checked) {
-      return null;
-    }
-  }
-
-  return (
-    <View
-      ref={ref}
-      aria-disabled={disabled}
-      aria-hidden={!(forceMount || checked)}
-      role={'presentation'}
-      {...props}
-    />
-  );
-});
 
 const useCheckboxContext = () => {
   const context = useContext(CheckboxContext);
@@ -94,7 +24,71 @@ const useCheckboxContext = () => {
     );
   }
   return context;
+};
+
+const CheckboxRoot = forwardRef<
+  ElementRef<typeof Pressable>,
+  ComponentPropsWithoutRef<typeof Pressable> & {
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+  disabled?: boolean;
 }
+>(({ disabled = false, checked, onCheckedChange, nativeID, ...props }, ref) => {
+  return (
+    <CheckboxContext.Provider
+      value={{ disabled, checked, onCheckedChange, nativeID }}
+    >
+      <CheckboxTrigger ref={ref} {...props} />
+    </CheckboxContext.Provider>
+  );
+});
+
+const CheckboxTrigger = forwardRef<
+  ElementRef<typeof Pressable>,
+  ComponentPropsWithoutRef<typeof Pressable>
+>(({ onPress: onPressProp, ...props }, ref) => {
+  const { disabled, checked, onCheckedChange, nativeID } = useCheckboxContext();
+
+  const onPress = (ev: GestureResponderEvent) => {
+    if (disabled) return;
+    const newValue = !checked;
+    onCheckedChange(newValue);
+    onPressProp?.(ev);
+  };
+
+  return (
+    <Pressable
+      ref={ref}
+      nativeID={nativeID}
+      aria-disabled={disabled}
+      role='checkbox'
+      aria-checked={checked}
+      onPress={onPress}
+      accessibilityState={{ checked, disabled }}
+      disabled={disabled}
+      {...props}
+    />
+  );
+});
+
+const CheckboxIndicator = forwardRef<
+  ElementRef<typeof View>,
+  ComponentPropsWithoutRef<typeof View> & { forceMount?: true }
+>(({ forceMount, ...props }, ref) => {
+  const { checked, disabled } = useCheckboxContext();
+
+  if (!forceMount && !checked) return null;
+
+  return (
+    <View
+      ref={ref}
+      aria-disabled={disabled}
+      aria-hidden={!(forceMount || checked)}
+      role='presentation'
+      {...props}
+    />
+  );
+});
 
 const Checkbox = forwardRef<
   ElementRef<typeof CheckboxRoot>,
@@ -104,7 +98,7 @@ const Checkbox = forwardRef<
     <CheckboxRoot
       ref={ref}
       className={cn(
-        'web:peer h-4 w-4 native:h-[20] native:w-[20] shrink-0 rounded-sm native:rounded border border-primary web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'web:peer h-4 w-4 native:h-[20] native:w-[20] shrink-0 rounded border border-primary web:ring-offset-background web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         props.checked && 'bg-primary',
         className
       )}
@@ -120,6 +114,6 @@ const Checkbox = forwardRef<
     </CheckboxRoot>
   );
 });
-Checkbox.displayName = "Checkbox";
+Checkbox.displayName = 'Checkbox';
 
 export { Checkbox };
