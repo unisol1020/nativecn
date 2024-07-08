c5d39d676ddf6adf576a2e962c580699024dfa25
unisol1020 <unisol1020@icloud.com>
2024-07-08 19:45:20 +0300

    [CLEANUP]

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,7 +6,7 @@ function main() {
   const program = new Command();
 
   program
-    .name('nativecn-ui')
+    .name('mono-ui')
     .description('add components and dependencies to your expo project')
     .version('0.0.6');
 
