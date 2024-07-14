7f4da1dece73d5284093376567f9e788ae704668
unisol1020 <unisol1020@icloud.com>
2024-07-14 15:11:39 +0300

    [WIP]

--- apps/docs/app/_layout.tsx
+++ apps/docs/app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import '../global.css';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme as NWUseColorScheme } from "nativewind";
