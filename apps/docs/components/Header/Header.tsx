8b2dcb4aa20eccea9782b2d7a5c6dbf5b55e7e67
unisol1020 <unisol1020@icloud.com>
2024-07-15 16:59:56 +0300

    [ADDED] logo to project

--- apps/docs/components/Header/Header.tsx
+++ apps/docs/components/Header/Header.tsx
@@ -1,10 +1,12 @@
-import {View} from 'react-native';
+import {View, Image, ImageSourcePropType} from 'react-native';
 import {Link} from 'expo-router';
 import {Button, H4, Text} from '@nativecn/ui';
 import {useColorScheme} from 'nativewind';
 import {BlurView} from 'expo-blur';
 import HeaderItem from '~/components/Header/HeaderItem';
 import {Moon, Sun} from 'lucide-react-native';
+import darkIcon from '../../assets/images/icon-dark.svg';
+import lightIcon from '../../assets/images/icon-light.svg';
 
 const items = [
   {
@@ -30,10 +32,15 @@ const Header = () => {
         className="p-3 flex flex-row items-center justify-between w-full"
       >
         <View
-          className="flex flex-row items-center w-full"
+          className="flex flex-row items-center w-full px-4"
         >
+          <Image
+            style={{ width: 16, height: 16, marginTop: 2 }}
+            source={(colorScheme === 'dark' ? darkIcon : lightIcon) as ImageSourcePropType}
+          />
+
           <Link asChild href="/">
-            <Button variant="link">
+            <Button style={{paddingLeft: 5}} variant="link">
               <H4>nativecn</H4>
             </Button>
           </Link>
