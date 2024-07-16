878b9d7b68a941edb7426b0fe0a51d54588c0c84
unisol1020 <unisol1020@icloud.com>
2024-07-16 14:50:55 +0300

    [ADDED] favicon and metas

--- apps/docs/app/+html.tsx
+++ apps/docs/app/+html.tsx
@@ -1,5 +1,5 @@
-import { ScrollViewStyleReset } from 'expo-router/html';
-import { type PropsWithChildren } from 'react';
+import { ScrollViewStyleReset } from "expo-router/html";
+import { type PropsWithChildren } from "react";
 
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
@@ -9,10 +9,24 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
+        <title>nativecn</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="application-name" content="nativecn" />
+        <meta name="og:type" content="website" />
+        <meta name="og:site_name" content="nativecn" />
+        <meta name="og:title" content="nativecn" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="nativecn" />
+        <meta
+          name="og:description"
+          content="A UI library of beautiful and customizable React Native components."
+        />
+        <meta name="og:url" content="https://nativecn.vercel.app" />
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
