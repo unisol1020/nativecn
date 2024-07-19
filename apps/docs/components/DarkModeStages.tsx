0a7016a07336d7d52b860992f4e5a3d2287cc841
unisol1020 <unisol1020@icloud.com>
2024-07-19 10:00:04 +0300

    [ADDED] Theme toggle docs

--- apps/docs/components/DarkModeStages.tsx
+++ apps/docs/components/DarkModeStages.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { View } from "react-native";
-import { P } from "@nativecn/ui";
+import { Button, Card, CardContent, Lead, P, Text } from "@nativecn/ui";
 import AutoTextGray from "./AutoTextGray";
 import Step from "~/components/Step";
 import CopyCard from "~/components/CopyCard";
+import { Link } from "expo-router";
+import { Info } from "lucide-react-native";
 
 const DarkModeStages = () => {
   return (
@@ -66,7 +68,7 @@ const DarkModeStages = () => {
               <AutoTextGray>
                 <AutoTextGray>
                   {
-                    "import { ThemeProvider } from '~/lib/ThemeProvider';\n\nconst RootLayout = () => {\n\t<ThemeProvider>\n\t\t<Stack>\n\t</ThemeProvider>\n};"
+                    "import { ThemeProvider } from '~/lib/ThemeProvider';\n\nconst RootLayout = () => {\n\t return (\n\t\t<ThemeProvider>\n\t\t\t<Stack>\n\t\t</ThemeProvider>\n\t);\n};"
                   }
                 </AutoTextGray>
               </AutoTextGray>
@@ -74,6 +76,45 @@ const DarkModeStages = () => {
           </CopyCard>
         </View>
       </Step>
+
+      <Step step={4}>
+        <P className="text-md font-medium pt-1">
+          Place a mode toggle on your app to toggle between light and dark mode:
+        </P>
+
+        <View className="flex flex-col gap-2">
+          <CopyCard>
+            <View className="rounded-lg flex flex-col">
+              <AutoTextGray>
+                <AutoTextGray>
+                  {
+                    "import { useColorScheme } from 'nativewind';\nimport { Button } from '@/components/Button';\nimport { Text } from \"@/components/Text\";\n\nconst ToggleComponent = () => {\n\tconst { toggleColorScheme } = useColorScheme();\n\n\treturn (\n\t\t <Button onPress={toggleColorScheme}>\n\t\t\t<Text>\n\t\t\t\tToggle Theme\n\t\t\t</Text>\n\t\t</Button>\n\t);\n};"
+                  }
+                </AutoTextGray>
+              </AutoTextGray>
+            </View>
+          </CopyCard>
+        </View>
+      </Step>
+
+      <Card className="w-full mt-4 ml-14">
+        <CardContent className="p-4 flex flex-row items-center">
+          <Text className="mr-4">
+            <Info />
+          </Text>
+
+          <Lead className="text-base">Reference:</Lead>
+
+          <Link
+            target="_blank"
+            href="https://www.nativewind.dev/v4/core-concepts/dark-mode#manual-color-scheme-control"
+          >
+            <Button variant="link">
+              <Text className="pt-[2px]">NativeWind docs</Text>
+            </Button>
+          </Link>
+        </CardContent>
+      </Card>
     </View>
   );
 };
