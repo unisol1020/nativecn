80dd60e155ee65c86fdcec832b483823d2b7e9e1
unisol1020 <unisol1020@icloud.com>
2024-09-27 13:34:53 +0300

    [ADDED] new docs next js 15 app

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
+};
+
+export default nextConfig;
---
+++ apps/docs/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nativecn",
+  description:
+    "Nativecn is a library of components and utilities for React Native.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
