82bda30583a35e4ca17476eb97171d0256c42d26
unisol1020 <unisol1020@icloud.com>
2024-10-04 17:50:31 +0300

    [WIP]

--- apps/docs/src/components/Header/Nav/NavItem.tsx
+++ apps/docs/src/components/Header/Nav/NavItem.tsx
@@ -1,13 +1,30 @@
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavItem = ({ name, href }: { name: string; href: string }) => {
   const pathname = usePathname();
-  const isActive = pathname ? pathname.includes(href) : false;
+  const isActive = pathname?.includes(href);
 
   return (
     <Link href={href}>
-      <span className={isActive ? 'active' : ''}>{name}</span>
+      <div
+        className={`relative text-sm font-light transition-colors duration-300 ease-in-out group px-2 py-1
+        ${
+          isActive
+            ? "text-foreground"
+            : "text-muted-foreground hover:text-foreground"
+        }`}
+      >
+        <span className="relative z-10">{name}</span>
+        <span
+          className={`absolute inset-0 bg-muted rounded-full transition-transform duration-300 ease-out origin-left
+          ${
+            isActive
+              ? "transform scale-x-100"
+              : "transform scale-x-0 group-hover:scale-x-100"
+          }`}
+        ></span>
+      </div>
     </Link>
   );
 };
