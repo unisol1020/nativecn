4642cddda9801b33f2dedea48182f9b4861692ac
unisol1020 <unisol1020@icloud.com>
2024-10-04 18:21:41 +0300

    [REMOVED] code highlight

--- apps/docs/src/components/CodeBlock/CodeBlock.tsx
+++ apps/docs/src/components/CodeBlock/CodeBlock.tsx
@@ -1,27 +1,9 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import CopyButton from "./CopyButton";
 
 const CodeBlock = ({ code, language }: { code: string; language: string }) => {
   return (
     <div className="relative max-h-[500px] overflow-auto">
       <CopyButton code={code} />
-
-      <SyntaxHighlighter
-        language={language}
-        style={vscDarkPlus}
-        className="rounded-md"
-        customStyle={{
-          padding: "1rem",
-          lineHeight: "1.5rem",
-          fontSize: "0.875rem",
-          backgroundColor: "#27272A",
-          color: "#d4d4d4",
-        }}
-        wrapLines
-      >
-        {code}
-      </SyntaxHighlighter>
     </div>
   );
 };
