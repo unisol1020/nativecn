104f289d26177efc4f08d9fe882d91d2ee58d062
unisol1020 <unisol1020@icloud.com>
2024-10-04 16:21:43 +0300

    [WIP]

---
+++ apps/docs/pages/404.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return <h1>404 - Page Not Found</h1>;
+};
+
+export default Custom404;
--- apps/docs/src/components/Header/Nav/NavItem.tsx
+++ apps/docs/src/components/Header/Nav/NavItem.tsx
@@ -1,30 +1,13 @@
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const NavItem = ({ name, href }: { name: string; href: string }) => {
   const pathname = usePathname();
-  const isActive = pathname?.includes(href);
+  const isActive = pathname ? pathname.includes(href) : false;
 
   return (
     <Link href={href}>
-      <div
-        className={`relative text-sm font-light transition-colors duration-300 ease-in-out group px-2 py-1
-        ${
-          isActive
-            ? "text-foreground"
-            : "text-muted-foreground hover:text-foreground"
-        }`}
-      >
-        <span className="relative z-10">{name}</span>
-        <span
-          className={`absolute inset-0 bg-muted rounded-full transition-transform duration-300 ease-out origin-left
-          ${
-            isActive
-              ? "transform scale-x-100"
-              : "transform scale-x-0 group-hover:scale-x-100"
-          }`}
-        ></span>
-      </div>
+      <span className={isActive ? 'active' : ''}>{name}</span>
     </Link>
   );
 };
