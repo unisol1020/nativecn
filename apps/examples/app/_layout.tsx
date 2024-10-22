d8e670f35034fd45c7a0955fba59904f464b2c40
unisol1020 <unisol1020@icloud.com>
2024-10-22 16:44:15 +0300

    [CLEANUP]

--- apps/examples/app/_layout.tsx
+++ apps/examples/app/_layout.tsx
@@ -4,7 +4,7 @@ import * as SplashScreen from "expo-splash-screen";
 import "../global.css";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { ThemeProvider } from "~/providers/ThemeProvider";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 SplashScreen.preventAutoHideAsync();
 
