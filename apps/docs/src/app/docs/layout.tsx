aa7604a89dcf50fa935b8e8b47893520928847aa
unisol1020 <unisol1020@icloud.com>
2024-12-11 18:52:45 +0200

    [UPDATE] adjust padding in DocsLayout component for improved responsiveness

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -6,7 +6,9 @@ const DocsLayout = ({ children }: PropsWithChildren) => {
     <div className="grid grid-cols-6 gap-4">
       <SideMenu className="hidden md:block z-[1] max-h-[calc(100vh-58px)] sticky top-[57px] pt-4" />
 
-      <div className="col-span-6 md:col-span-5 pt-4 px-10">{children}</div>
+      <div className="col-span-6 md:col-span-5 pt-4 px-0 md:px-10">
+        {children}
+      </div>
     </div>
   );
 };
