4c709ff360fbfd64ec92350d7925ca924478cdb1
unisol1020 <unisol1020@icloud.com>
2024-12-11 18:54:01 +0200

    UPDATE: enhance SideMenu component styling by adjusting border properties in layout

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 const DocsLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="grid grid-cols-6 gap-4">
-      <SideMenu className="hidden md:block z-[1] max-h-[calc(100vh-58px)] sticky top-[57px] pt-4" />
+      <SideMenu className="hidden md:block z-[1] max-h-[calc(100vh-58px)] sticky top-[57px] pt-4 border-r" />
 
       <div className="col-span-6 md:col-span-5 pt-4 px-0 md:px-10">
         {children}
--- apps/docs/src/components/SideMenu/SideMenu.tsx
+++ apps/docs/src/components/SideMenu/SideMenu.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 const SideMenu = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("overflow-y-auto border-r pr-4 ", className)}>
+    <div className={cn("overflow-y-auto pr-4 ", className)}>
       <div className="flex flex-col gap-4 mx-7">
         {MENU_ITEMS.map((item) => (
           <SideMenuItem key={item.name} {...item} />
