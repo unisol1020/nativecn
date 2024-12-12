69ee25cf867a7ae6fd736766ab0778759d6b0d17
unisol1020 <unisol1020@icloud.com>
2024-12-12 09:43:10 +0200

    UPDATE: adjust padding in DocsLayout and Layout components for improved spacing

--- apps/docs/src/app/docs/layout.tsx
+++ apps/docs/src/app/docs/layout.tsx
@@ -6,7 +6,7 @@ const DocsLayout = ({ children }: PropsWithChildren) => {
     <div className="grid grid-cols-6 gap-4">
       <SideMenu className="hidden md:block z-[1] max-h-[calc(100vh-58px)] sticky top-[57px] pt-4 border-r" />
 
-      <div className="col-span-6 md:col-span-5 pt-4 px-0 md:px-10">
+      <div className="col-span-6 md:col-span-5 pt-4 px-0 md:px-10 pb-5">
         {children}
       </div>
     </div>
